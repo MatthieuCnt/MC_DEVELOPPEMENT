9b606b1437cc2a3bf34748d45cb7b8c367b7bfa8
MatthieuCnt <74907882+MatthieuCnt@users.noreply.github.com>
2023-01-25 18:39:39 +0100

    nav linkedin

---
+++ index.js
@@ -0,0 +1,9 @@
+function navbar() {
+    nav = document.getElementById('nav_div');
+    nav.style.display = 'block';
+    
+  /* nav = document.createElement('div');
+    nav.className = 'style_nav';
+    nav.id = "navbar_id";
+    document.getElementById('nav_div').appendChild(nav);*/
+}
