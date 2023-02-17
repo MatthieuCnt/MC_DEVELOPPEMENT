78a1bc9549c707486db3a9244a18a9e8340c1cf0
MatthieuCnt <74907882+MatthieuCnt@users.noreply.github.com>
2023-02-17 17:08:32 +0100

    changes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function navbar() {
-    nav = document.getElementById('nav_div');
-    nav.style.display = 'block';
-    
-  /* nav = document.createElement('div');
-    nav.className = 'style_nav';
-    nav.id = "navbar_id";
-    document.getElementById('nav_div').appendChild(nav);*/
+// Nav responsive
+function editNav() {
+	var x = document.getElementById('myTopnav');
+	if (x.className === 'topnav') {
+		x.className += ' responsive';
+	} else {
+		x.className = 'topnav';
+	}
 }
